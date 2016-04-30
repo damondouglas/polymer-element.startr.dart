19765ae94c00e006a04c3e3a372b098a7baa9b9f
Damon Douglas <douglas.damon@gmail.com>
2016-04-30 14:51:08 -0700

    Initial commit

---
+++ lib/__element_file_name__.dart
@@ -0,0 +1,16 @@
+library __element_file_name__;
+
+@HtmlImport('__element_file_name__.html')
+import 'dart:html';
+import 'package:web_components/web_components.dart' show HtmlImport;
+import 'package:polymer/polymer.dart';
+
+@PolymerRegister('__element_tag_name__')
+class __elementclass__ extends PolymerElement {
+  __elementclass__.created() : super.created();
+
+  attached() {
+
+  }
+
+}
