7ada41cce4910731e6d73b177049299b5ec97c00
Damon Douglas <douglas.damon@gmail.com>
2016-04-30 14:55:21 -0700

    startr doesn't like __xxx_xxx__

--- lib/__elementfile__.dart
+++ lib/__elementfile__.dart
@@ -1,11 +1,11 @@
-library __element_file_name__;
+library __elementfile__;
 
-@HtmlImport('__element_file_name__.html')
+@HtmlImport('__elementfile__.html')
 import 'dart:html';
 import 'package:web_components/web_components.dart' show HtmlImport;
 import 'package:polymer/polymer.dart';
 
-@PolymerRegister('__element_tag_name__')
+@PolymerRegister('__elementname__')
 class __elementclass__ extends PolymerElement {
   __elementclass__.created() : super.created();
 
